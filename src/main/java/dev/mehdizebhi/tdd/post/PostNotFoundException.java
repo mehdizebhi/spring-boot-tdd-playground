a1163662b9e82b4a76d1987d87429d85202780b5
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-12 20:05:35 +0330

    update : add NotFound Exception for post

---
+++ src/main/java/dev/mehdizebhi/tdd/post/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.mehdizebhi.tdd.post;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PostNotFoundException extends RuntimeException {
+}
