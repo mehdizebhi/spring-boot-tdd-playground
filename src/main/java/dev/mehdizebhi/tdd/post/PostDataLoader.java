1557289d3f0090481c8d2fec5aecb72e4c628d56
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-12 20:04:01 +0330

    update : add post data loader from posts.json file

---
+++ src/main/java/dev/mehdizebhi/tdd/post/PostDataLoader.java
@@ -0,0 +1,41 @@
+package dev.mehdizebhi.tdd.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.asm.TypeReference;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Component
+public class PostDataLoader implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(PostDataLoader.class);
+    private final ObjectMapper objectMapper;
+    private final PostRepository postRepository;
+
+    public PostDataLoader(ObjectMapper objectMapper, PostRepository postRepository) {
+        this.objectMapper = objectMapper;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (postRepository.count() == 0) {
+            String POSTS_JSON = "/data/posts.json";
+            log.info("Loading posts into database from JSON: {}", POSTS_JSON);
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream(POSTS_JSON)){
+                Posts data = objectMapper.readValue(inputStream, Posts.class);
+                postRepository.saveAll(data.posts);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read JSON data", e);
+            }
+        }
+    }
+
+    record Posts(List<Post> posts) {}
+}
