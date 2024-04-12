fb55eeb003169f01b107cae804f5ceb1f9f750f6
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-12 10:10:27 +0330

    add : create new spring boot project with dependency

---
+++ src/test/java/dev/mehdizebhi/tdd/SpringBootTddPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mehdizebhi.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTddPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
